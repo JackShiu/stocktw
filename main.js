718dfc435292eb7329733abf5d478efdda15917c
JackShiu <poming.shiu@gmail.com>
2018-06-19 20:12:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -500,7 +500,12 @@ var calculateAll = async(stockID,options ) => {
 	console.log("計算全部");
 	let storeString ={};
 	// 抓取所有股票
-	const data = await getTWStockList(stockID);
+	let data = await getTWStockList(stockID);
+	//travelingBeginIndex
+	data = data.slice(options.travelingBeginIndex);
+	//console.log(data)
+	//return ;
+	
 	// 遍例抓取的股票陣列
 	/* promise 序列化函數 */ 
 	const promiseSerial = funcs =>
@@ -514,7 +519,8 @@ var calculateAll = async(stockID,options ) => {
 			let listItem = (await req).slice(-1);
 			let accId = listItem[0] === undefined ? undefined : listItem[0].id
 			// console.log(accId);
-			if(accId ===undefined || accId != -1 && accId < options.maxCount-1){
+			//if(accId ===undefined || accId != -1 && accId < options.maxCount-1){
+			if(accId ===undefined || accId != -1){
 				let value = await evaluate(stock,options);
 				/*除非要求存檔全部，不然只存有效資料*/
 				if(options.allData|value.valid){
@@ -540,12 +546,14 @@ var calculateAll = async(stockID,options ) => {
 	program
 		// .allowUnknownOption()
 		.usage('<stockID,all> [options]')
-		.option('-s, --save', '運算玩後進行存檔')
+		.option('-s, --save', '運算完後進行存檔')
 		.option('-o, --save-override', '存檔方式使用"覆寫"')
 		.option('-p, --save-append', '存檔方式使用"附加"(default)')
 		.option('-a, --save-availableOnly', '(all才有用)只存有完整算完的資料(default)')
-		.option('-l, --save-allData', '(all才有用)除存所以抓取資料')
-		.option('-c, --count <value>', '(all才有用)遍例的有效上限(default:10)')
+		.option('-l, --save-allData', '(all才有用)存檔所有抓取到資料(包含沒完整)')
+		.option('-c, --count <value>', '(all才有用)遍歷抓取有效資料的上限(default:10)')
+		.option('-b, --traveling-begin <value>', '(all才有用)設定遍歷起始點')
+		//.option('-f, --final-sort <value>', '(all才有用)遍歷完後排序的條件')
 		.option('!, --debug', '開啟debug mode')
 		.arguments('<cmd> [env]')
 		.action(function (cmd) {
@@ -563,22 +571,42 @@ var calculateAll = async(stockID,options ) => {
 			append:program.saveAppend,
 			maxCount:program.count ||10,
 			availableOnly:program.saveAvailableOnly||true,
-			allData:program.saveAllData
+			allData:program.saveAllData,
+			travelingBeginIndex: undefined // program.travelingBegin
 		};
 	if (program.debug) DBG=true;
 	// if (options.save) console.log('  - save');
 	// if (options.override) console.log('  - override');
 	// if (options.append) console.log('  - append');
+	//if (options.travelingBegin) console.log('  - travelingBegin'+options.travelingBegin);
 	// let timeformat = (val) => ('0'+val).substr(-2); //自動補零
 
 	if(func === undefined) return -1;
 	
-	//TODO:判斷有stockID 那是不是有效的股票
-	if(stockID != undefined && (await getTWStockList()).indexOf(parseInt(stockID))===-1){
+	//判斷有stockID 那是不是有效的股票
+	const stockList = await getTWStockList();
+	if(stockID != undefined && stockList.indexOf(parseInt(stockID))===-1){
 		console.log(`沒有輸入的stock [${stockID}]`);
 		return -1;
 	}
+	
+	//判斷travelingBegin那是不是有效的股票，不是就從最近的股票開始
 
+	if(program.travelingBegin != undefined){
+		let tempVal = program.travelingBegin;
+		let tempIndex = stockList.indexOf(parseInt(tempVal));
+		if(parseInt(tempVal) > 0 && tempIndex ===-1){
+			let closest = stockList.reduce( (prev, curr) => {
+				return (Math.abs(curr - tempVal) < Math.abs(prev - tempVal) ? curr : prev);
+			});
+			console.log(`最靠近的股票是(${closest})`);
+			tempIndex = stockList.indexOf(parseInt(closest));
+		} else {
+			console.log(`遍歷起點是(${tempVal})`);
+		}
+		options.travelingBeginIndex = tempIndex;
+	}
+	
 	//呼叫評估函數
 	let storeData = await func(stockID,options);
 
@@ -591,6 +619,3 @@ var calculateAll = async(stockID,options ) => {
 	}
 
 })();
-
-
-
