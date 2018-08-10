a0e949b67f49bd784dd89cd03f5af1dbd64a2049
JackShiu <poming.shiu@gmail.com>
2018-08-11 00:08:21 +0800

    update app.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { stockInfo} from 'interface/stockInfo';
+import { stockInfo as stockInfoType} from 'interface/stockInfo';
 import  * as stockData from 'data/all_stock_value.json';
 import  * as stockList from 'data/list.json';
+
+
+
 import { calculate} from 'parser/analysis/calculate'
 class App extends Component {
   constructor(props){
@@ -10,68 +13,62 @@ class App extends Component {
 
   }
 
-
   render() {
-    // console.log(alldata);
-    let display = stockList.data.map(req => {
-      let stock = stockData[req.ID];
-      let info = new stockInfo(stock);
-      // console.log(calculate(info).RiskEarningRatio);
-        let stylesContainer = {
-          width: '800px',
-          margin: '0 auto',
-          // backgroundColor: '#8CC152'
-        }
-      let stylesItem = {
-        display: "inline-block",
-        width: '100px',
-        marginRight: '20px',
-        backgroundColor: '#aaaa'
-      };
-      let getDisplay = (stockID = "NULL", info = {}, cal = {}) =>
-      <div>
-      <div>=======[${stockID}]========</div>
-      <div>股票：${info.getStockName()}</div>
-      <div>營收比重：${info.getProductType()}</div>
-      <div>收盤價: ${info.getPrice('D_closingPrice')} (元)</div>
-      <div>預估本益比: ${cal.PredictPE[0].toFixed(2)}~${cal.PredictPE[1].toFixed(2)}</div>
-      <div>過去本益比區間(年): 高:[${info.getPE("Y_max").slice(0, 6)}] 低:[${info.getPE("Y_min").slice(0, 6)}]</div>
-      <div>預估營收年增率: ${cal.predictProfitMonthYoY.toFixed(2)} (%)</div>
-      <div>  (過去六個月 [${info.getOperatingRevenue('M_YoY').slice(0, 5)}])</div>
-      <div>預估營收: ${cal.PredictedEarning.toFixed(2)} (百萬)</div>
-      <div>預估稅後淨利率: ${(cal.PredictProfitRatio * 100).toFixed(2)} (%)</div>
-      <div>預估EPS: ${cal.PredictEPS.toFixed(2)} (元) (過去兩年PES=[${info.getEPS('Y_value').slice(0, 2)}])</div>
-      <div>預估股價高低落點: ${cal.PredictHighestPrice.toFixed(2)}~${cal.PredictLowestPrice.toFixed(2)} ,(當前:${info.getPrice('D_closingPrice')})</div>
-      <div>預估報酬率: ${cal.PredictEarningRatio.toFixed(2)}</div>
-      <div>預估風險: ${cal.PredictLossRatio.toFixed(2)}</div>
-      <div>風險報酬倍數: ${cal.RiskEarningRatio.toFixed(2)}</div>
-      <div>PEG : ${cal.PEG.toFixed(3)}\n</div>
-      </div>;
-      return (
-        <div key={req.ID} style={stylesContainer}>
-          <div className="item-header" >
-            <span style={stylesItem}>{req.ID}</span>
-            <span style={stylesItem}>{req.Name}</span>
-            <span style={stylesItem}>{req.Type}</span>
-            <span>{req.Category}</span>
-          </div>
-          <div className="item-content">
-            {getDisplay(req.ID, info, calculate(info))}
-          </div>
-        </div>
-      )
-    });
-    return <div className="App">
-        <div>
-          <h1>Header</h1>
-        </div>
-      <div>
-          {display}
-        </div>
+    return (
+      <div className="App">
+          <header id="header">
+            <h1 id="header-title">stocktw</h1>
+          </header>
+          <section id="stock-list">
+          {getStockInfo.sort((a, b) => b.rank - a.rank).map(req => stockInfoDisplay(req))}
+          </section>
+          <footer id="footer">
+           <span>STOCKTW</span> <span>Power by <b>jackShiu</b></span>
+          </footer>
       </div>
+    )
   }
 }
+//.sort((a, b) => b.rank - a.rank)
+let getStockInfo = stockList.data.map(req => {
+  let stock= stockData[req.ID];
+  let info= new stockInfoType(stock);
+  let cal= calculate(info);
+  let rank= cal.RiskEarningRatio;
+  return (
+    {
+      req,
+      info,
+      cal,
+      rank
+    }
+  )
+});
 
-
+let stockInfoDisplay = ({ req, cal, info}) => (
+    <div key={req.ID} className="stock-item">
+      <div className="stock-item__head clearfix">
+        <div className="width_10">{req.ID}</div>
+        <div className="width_40">{req.Name}</div>
+        <div className="width_10">{req.Type}</div>
+        <div className="width_40">{req.Category}</div>
+      </div>
+      <div className="stock-item__description">
+        {info.getProductType()}
+      </div>
+      <div className="stock-item__info">
+        <li>收盤價: {info.getStockName()} (元)</li>
+        <li>預估本益比: {cal.PredictPE[0]}~{cal.PredictPE[1]}</li>
+        <li>預估營收年增率: {cal.predictProfitMonthYoY} (%)</li>
+        <li>預估營收: {cal.PredictedEarning} (百萬)</li>
+        <li>預估稅後淨利率: {cal.PredictProfitRatio * 100} (%)</li>
+        <li>預估EPS: {cal.PredictEPS} (元)</li>
+        <li>預估股價高低落點: {cal.PredictHighestPrice}~{cal.PredictLowestPrice}</li>
+        <li>預估報酬率: {cal.PredictEarningRatio}</li>
+        <li>預估風險: {cal.RiskEarningRatio}</li>
+        <li>風險報酬倍數: {cal.PEG}</li>
+      </div>
+    </div>
+)
 
 export default App;
